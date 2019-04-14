ead9af03cad5fe2cc454501bb203819030ec46b1
Hassan <hassanharga@gmail.com>
2019-04-14 22:38:42 +0200

    done signup

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { FormGroup, FormBuilder, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
-
+  signupForm: FormGroup;
+  errorMessage: string;
+  constructor(private authSerice: AuthService, private fb: FormBuilder) { }
+  signupUser() {   // create user
+    console.log(this.signupForm.value);
+    this.authSerice.createUser(this.signupForm.value).subscribe(
+      data => {
+        console.log(data);
+        this.signupForm.reset();
+      },
+      err => {
+        console.log(err);
+        if (err.error.msg) {
+          this.errorMessage = err.error.msg[0].message;
+        }
+        if (err.error.message) {
+          this.errorMessage = err.error.message;
+        }
+      }
+    );
+  }
   ngOnInit() {
+    this.signupForm = this.fb.group({ // validating form
+      username: ['', Validators.required],
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', Validators.required]
+    });
   }
 
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  url = 'http://localhost:8080/api/chatapp';
+  constructor(private http: HttpClient) { }
+  createUser(body): Observable<any> {
+    return this.http.post<any>(`${this.url}/register`, body);
+  }
+
+}
