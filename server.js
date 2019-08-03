e2a5c2da214c3845740fd3b22155262d27a9806d
Hassan <hassanharga@gmail.com>
2019-08-03 20:42:53 +0200

    change dist

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(__dirname + '/dist/task'))
 app.listen(process.env.PORT || '3000');
 
 //PathLocationStrategy
