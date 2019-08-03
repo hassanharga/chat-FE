110bdfbe35c754b5eee08953f0c1fc3aab6b5e3a
Hassan <hassanharga@gmail.com>
2019-08-03 20:39:09 +0200

    change dist

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.listen(process.env.PORT || '3000');
 //PathLocationStrategy
 
 app.get('/*', (req, res)=>{
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/task/index.html'));
 })
 
 console.log('server listening on port 3000')
