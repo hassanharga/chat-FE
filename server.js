f2b21dbaa9e594295c7055001bbdb4a0143e0527
Hassan <hassanharga@gmail.com>
2019-08-03 20:05:57 +0200

    change port

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 app.use(express.static(__dirname + '/dist'))
-let port = process.env.port || 3000;
-app.listen(port);
+// let port = process.env.port || 3000;
+app.listen(process.env.port || 3000);
 
 //PathLocationStrategy
 
